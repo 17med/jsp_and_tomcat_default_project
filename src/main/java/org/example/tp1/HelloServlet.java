7863b560c8e7ec89f5508341dacd2e9c4db0cfa4
17med <ahmedbenachour2003@gmail.com>
2024-09-27 22:47:35 +0100

    Initial commit

---
+++ src/main/java/org/example/tp1/HelloServlet.java
@@ -0,0 +1,29 @@
+package org.example.tp1;
+
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloServlet", value = "/hello")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
